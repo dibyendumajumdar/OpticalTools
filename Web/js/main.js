b0964506ba9f090b6f79593cfe11a03c67c0dd5e
Dibyendu Majumdar <mobile@majumdar.org.uk>
2020-05-03 15:37:12 +0100

    Create a simple svg rect

--- Web/js/main.js
+++ Web/js/main.js
@@ -1,3 +1,11 @@
-d3.select("#content")
-  .append("p")
-  .text("hello world");
+let svgContent = d3.select("#content")
+  .append("svg")
+  .attr("width", 400)
+  .attr("height", 400);
+
+// Draw rectangle
+svgContent.append("rect")
+  .attr("x", 10)
+  .attr("y", 10)
+  .attr("width", 100)
+  .attr("height", 100);
